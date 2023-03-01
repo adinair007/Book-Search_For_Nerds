80c27486f98ea9767f0ec6dfdb5b4511a94604fb
Aditya Nair <adinair.in@gmail.com>
2023-03-01 15:22:14 -0600

    Completed typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,19 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Profile {
+  type User {
     _id: ID
-    name: String
+    username: String
     email: String
     password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+  authors: [String]
+  description: String!
+  bookId: String!
+  image: String!
+  link: String!
+  title: String!
+  }
+
+  type Auth {
+  token: ID!
+  user: User
+  }
+
+  input SaveBookInput {
+  authors: [String]
+  description: String!
+  bookId: String!
+  image: String!
+  link: String!
+  title: String!
   }
 
   type Query {
-
+    me: User
   }
 
   type Mutation {
-    
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: SaveBookInput): User
+    deleteBook(bookId: ID!): User
   }
 `;
 
