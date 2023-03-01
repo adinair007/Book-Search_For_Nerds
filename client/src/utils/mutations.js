4b6fe9dc1c1965ea019c2d285c73d93f389eb1a7
Aditya Nair <adinair.in@gmail.com>
2023-03-01 16:18:23 -0600

    Completed queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -43,7 +43,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const DELETE_BOOK = gql`
+export const REMOVE_BOOK = gql`
   mutation deleteBook($bookId: ID!) {
     deleteBook(bookId: $bookId) {
       _id
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,20 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
+export const GET_ME = gql`
   query me {
     me {
       _id
-      name
-      skills
+      email
+      username
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
     }
   }
 `;
