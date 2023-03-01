33823c1db6f5dd0ba694acbc3e48401202506f09
Aditya Nair <adinair.in@gmail.com>
2023-03-01 14:56:29 -0600

    Refactored server.js to work with Apollo server & GraphQl. Created schemas template

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,21 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Profile {
+    _id: ID
+    name: String
+    email: String
+    password: String
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+    
+  }
+`;
+
+module.exports = typeDefs;
+
